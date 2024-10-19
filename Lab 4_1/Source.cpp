3bca866d19f428decd0a4462dbcd10c4bdb98fc5
ShiroTW <novosilskiyv@gmail.com>
2024-10-19 12:11:48 +0300

    Лабораторна 4.1

---
+++ Lab 4_1/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int N, i;
+    double S;
+    cout << "N = "; cin >> N;
+
+    S = 0;
+    i = N;
+    while (i <= 20)
+    {
+        S += (cos(i) + sin(i)) / (1 + cos(i) * sin(i));
+        i++;
+    }
+    cout << S << endl;
+
+    S = 0;
+    i = N;
+    do {
+        S += (cos(i) + sin(i)) / (1 + cos(i) * sin(i));
+        i++;
+    } while (i <= 20);
+    cout << S << endl;
+
+    S = 0;
+    for (i = N; i <= 20; i++)
+    {
+        S += (cos(i) + sin(i)) / (1 + cos(i) * sin(i));
+    }
+    cout << S << endl;
+
+    S = 0;
+    for (i = 20; i >= N; i--)
+    {
+        S += (cos(i) + sin(i)) / (1 + cos(i) * sin(i));
+    }
+    cout << S << endl;
+
+    return 0;
+}
